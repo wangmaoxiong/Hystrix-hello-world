852c5808afcbfd6fe63af0e3b6a7db8a9b48fed5
wangmaoxiong <2268461750@qq.com>
2020-02-16 12:00:00 +0800

    更新

--- gateway-zuul/src/main/java/com/wmx/gatewayzuul/GatewayZuulApplication.java
+++ gateway-zuul/src/main/java/com/wmx/gatewayzuul/GatewayZuulApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @EnableZuulProxy：开启网关代理，默认未开启。它是一个组合注解，包含了 @EnableCircuitBreaker
+ */
 @SpringBootApplication
 @EnableZuulProxy
 public class GatewayZuulApplication {
