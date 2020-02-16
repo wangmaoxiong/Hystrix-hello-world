d42fe813b4811fc56cab62def12af87c20107363
wangmaoxiong <2268461750@qq.com>
2020-02-16 10:17:04 +0800

    网关

---
+++ gateway-zuul/src/main/java/com/wmx/gatewayzuul/GatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.wmx.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+
+}
