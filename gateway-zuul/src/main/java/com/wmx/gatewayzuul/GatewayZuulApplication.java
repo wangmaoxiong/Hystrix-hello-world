d1783e87d113cc1ac957672f27cd8701a5b73d25
wangmaoxiong <2268461750@qq.com>
2020-02-16 11:31:46 +0800

    增加注释

--- gateway-zuul/src/main/java/com/wmx/gatewayzuul/GatewayZuulApplication.java
+++ gateway-zuul/src/main/java/com/wmx/gatewayzuul/GatewayZuulApplication.java
@@ -2,8 +2,10 @@ package com.wmx.gatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayZuulApplication {
 
     public static void main(String[] args) {
