d7fb5c0be10263863d21c54f4dacdcb6883fb153
wangmaoxiong <2268461750@qq.com>
2020-02-26 11:29:58 +0800

    更新

--- eureka_client2020/src/main/java/com/wmx/eureka_client2020/controller/PersonController.java
+++ eureka_client2020/src/main/java/com/wmx/eureka_client2020/controller/PersonController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
 import java.util.UUID;
 
 @RestController
@@ -20,7 +24,7 @@ public class PersonController {
      * @return
      */
     @GetMapping("zebra/person/info")
-    public String info(String id) {
+    public String info(String id, HttpServletRequest request, HttpServletResponse response) {
         JsonNodeFactory jsonNodeFactory = JsonNodeFactory.instance;
         ObjectNode objectNode = jsonNodeFactory.objectNode();
         objectNode.put("code", 200);
@@ -43,6 +47,23 @@ public class PersonController {
         objectNode.put("cipher", UUID.randomUUID().toString());
         objectNode.put("timeStamp", System.currentTimeMillis());
         return objectNode.toString();
+    }
+
+    /**
+     * http://localhost:9394/zebra/testSensitive
+     *
+     * @param response
+     * @return
+     */
+    @GetMapping("zebra/testSensitive")
+    public String testSensitive(HttpServletRequest request, HttpServletResponse response) {
+        String cookie_header = request.getHeader("Cookie");
+        System.out.println("cookie_header=" + cookie_header);
+
+        Cookie cookie1 = new Cookie("token", "1582616408");
+        response.addCookie(cookie1);
+
+        return cookie1.toString();
     }
 
 }
